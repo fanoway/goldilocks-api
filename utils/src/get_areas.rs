72c456fe35c95f8ba335812ba0dba90044f6fe46
fanoway <86997883+fanoway@users.noreply.github.com>
2023-03-17 18:42:21 -0700

    write areas to redis

--- utils/src/get_areas.rs
+++ utils/src/get_areas.rs
@@ -1,6 +1,8 @@
 use dotenv::dotenv;
 use gql_client::{Client, ClientConfig};
+use redis::Commands;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Area {
@@ -11,17 +13,25 @@ struct Area {
 #[derive(Deserialize, Serialize, Debug)]
 struct Metadata {
     lat: f64,
-    long: f64,
+    lng: f64,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Data {
-    areas: NodeList<Area>,
+    areas: Vec<Area>,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
-struct NodeList<T> {
-    data: Vec<T>,
+// #[derive(Deserialize, Serialize, Debug)]
+// struct Areas<T> {
+//     areas: Vec<T>,
+// }
+
+async fn add_area(con: &mut redis::Connection, area: &Area) -> redis::RedisResult<()> {
+    let _: () = con.set(
+        &area.area_name,
+        redis::ToRedisArgs::to_redis_args(&json!(area.metadata).to_string()),
+    )?;
+    Ok(())
 }
 
 #[tokio::main]
@@ -29,8 +39,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Load env variables
     dotenv().ok();
 
-    // Connect to redis to write data to it
-    let redis_url = std::env::var("REDIS_URL").expect("REDIS_URL must be set.");
+    // Connect to redis so we can write data to it
+    let redis_url = std::env::var("REDIS").expect("REDIS_URL must be set.");
     let redis_client = match redis::Client::open(redis_url) {
         Ok(val) => Ok(val),
         Err(val) => {
@@ -39,9 +49,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }?;
 
-    let redis_connection = redis_client.get_connection()?;
+    let mut redis_connection = redis_client.get_connection()?;
 
-    let query = "query Locations {
+    // Get areas from openbeta
+    let query = r#"query Locations {
         areas {
           area_name
             metadata {
@@ -50,7 +61,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
           }
       }
-      ";
+      "#;
 
     let client_config = ClientConfig {
         endpoint: "https://api.openbeta.io".to_string(),
@@ -63,19 +74,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let response = match client.query::<Data>(query).await {
         Ok(option) => match option {
             Some(val) => Ok(val),
-            None => Err("Error".to_string()),
+            None => Err("Error1".to_string()),
         },
         Err(val) => {
-            println!("{}", val.to_string());
+            // println!("{}", val.to_string());
             Err(val.to_string())
         }
     }?;
 
-    for area in &response.areas.data {
-        println!(
-            "{} | {} | {}",
-            area.area_name, area.metadata.lat, area.metadata.long
-        );
+    for area in &response.areas {
+        add_area(&mut redis_connection, area).await?;
+        // println!(
+        //     "{} | {} | {}",
+        //     area.area_name, area.metadata.lat, area.metadata.lng
+        // );
     }
 
     Ok(())
