098f6e1caa8232f92dc09fce9dedf20ab6bac8c5
fanoway <86997883+fanoway@users.noreply.github.com>
2023-03-19 17:57:59 -0700

    axum beginnings

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,11 +1,45 @@
-use rocket::{get, launch, routes};
+mod weather_data_model;
+use axum::{
+    extract::{Path, Query},
+    routing::{get, post},
+    Router,
+};
+use serde::{Deserialize, Serialize};
 
-#[get("/v1/areas/<name>")]
-fn hello(name: &str) -> String {
-    format!("Area -{}!", name)
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(process_weather));
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "axum_api=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .expect("Failed to start server");
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+#[derive(Deserialize, Debug)]
+struct LatLngParams {
+    lat: f64,
+    lng: f64,
+}
+
+async fn process_weather(Query(latlng): Query<LatLngParams>) -> Result<(), String> {
+    return Err(format!("{:?}", latlng));
+
+    let weather_raw = match weather_data_model::get_weather_from_api(latlng.lat, latlng.lng).await {
+        Ok(val) => Ok(val),
+        Err(_) => Err("Error parsing json".to_string()),
+    }?;
+    match weather_data_model::add_weather_to_db(weather_raw).await {
+        Ok(_) => Ok(()),
+        Err(_) => Err("Error writing to db".to_string()),
+    }
 }
